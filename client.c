9c76167241efebd296c8b3113e0753fff073c994
shahid!! <shahid10b2564@gmail.com>
2026-08-08 17:45:37 +0530

    Add files via upload

--- client.c
+++ client.c
@@ -1,43 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#define PORT 8081
-#define BUFFER_SIZE 1024
-int main()
-{
-    int sock = 0;
-    struct sockaddr_in serv_addr;
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h> 
+#include <arpa/inet.h> 
+#define PORT 8080 
+#define BUFFER_SIZE 1024 
+int main() { 
+    int sock = 0; 
+    struct sockaddr_in serv_addr; 
+    char reg_no[BUFFER_SIZE]; 
     char buffer[BUFFER_SIZE];
-    char input[BUFFER_SIZE];
-    // Create socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("Socket creation␣error\n");
-        return -1;
-    }
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    // Change IP here if using different machine
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
-    {
-        printf("Invalid address\n");
-        return -1;
-    }
-    // Connect to server
-    
+     // 1. Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) ;
+   // 3. Connect to server
     connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    printf("Connected to server.\n");
-    while (1)
-    {
-        printf("\nEnter command(UP|text, LOW|text,REV| text) or ’bye’:\n");
-        fgets(input, BUFFER_SIZE, stdin);
-        send(sock, input, strlen(input), 0);
-        memset(buffer, 0, BUFFER_SIZE);
-        read(sock, buffer, BUFFER_SIZE);
-        printf("Server response:␣%s\n", buffer);
-    }
-    close(sock);
-    return 0;
+
+    while (1) {
+        printf("\nEnter Registration Number(or type ’ bye’ to exit): "); 
+        fgets(reg_no, BUFFER_SIZE, stdin); 
+        reg_no[strcspn(reg_no, "\n")] = 0; 
+        send(sock, reg_no, strlen(reg_no), 0); 
+        // Exit condition 
+        if (strcmp(reg_no, "bye") == 0){
+            printf("Connection closed.\n"); 
+            break; 
+        } 
+        memset(buffer, 0, BUFFER_SIZE); 
+        read(sock, buffer, BUFFER_SIZE); 
+        printf("Server Response: %s\n", buffer); 
+    } 
+    close(sock); 
+    return 0; 
 }
--- server.c
+++ server.c
@@ -1,109 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <ctype.h>
-#define PORT 8081
-#define BUFFER_SIZE 1024
-// Convert to uppercase using ASCII
-void to_upper(char *str)
-{
-    int i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] = str[i] - 32;
-            i++;
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h> 
+#include <arpa/inet.h> 
+#define PORT 8080 
+#define BUFFER_SIZE 1024 
+void find_student(char *reg_no, char *response) { 
+    FILE *fp = fopen("students.txt", "r"); 
+    if (fp == NULL) { 
+        strcpy(response, "Error␣opening␣file"); 
+        return; 
+    } 
+    char file_reg[50], name[50], branch[50], college [100]; 
+    int found = 0; 
+    while (fscanf(fp, "%s %s %s %s", file_reg, name, branch, college) != EOF) { 
+        if (strcmp(file_reg, reg_no) == 0) { 
+            sprintf(response, "Name:%s,Branch:%s, College:%s", name, branch, college); 
+            found = 1; 
+            break; 
         }
     }
-}
-// Convert to lowercase using ASCII
-void to_lower(char *str)
-{
-    int i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] = str[i] + 32;
-            i++;
-        }
-    }
-}
-// Function to reverse string
-void reverse(char *str)
-{
-    int i;
-    int len = strlen(str);
-
-    for (i = 0; i < len / 2; i++)
-    {
-        char temp = str[i];
-        str[i] = str[len - i - 1];
-        str[len - i - 1] = temp;
-    }
-}
-int main()
-{
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE];
-    // Create socket
+    if (!found) { 
+        strcpy(response, "Data not found"); 
+    } 
+    fclose(fp); 
+} 
+int main() { 
+    int server_fd, new_socket; 
+    struct sockaddr_in address; 
+    int addrlen = sizeof(address); 
+    char reg_no[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE]; 
+    char response[BUFFER_SIZE]; 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0)
-    {
-        perror("Socket␣failed");
-        exit(EXIT_FAILURE);
-    }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
-    // Bind
+
+    // 3. Bind socket
     bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-
-    // Listen
+    // 4. Listen for connections
     listen(server_fd, 3);
-
     printf("Server waiting on port %d...\n", PORT);
+     // 5. Accept connection
+    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+    
+    while (1) { 
+        // 6. Read from client
+    read(new_socket, buffer, 1024);
+    printf("Client: %s\n", buffer);
 
-    // Accept connection
-    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-    printf("Client␣connected.\n");
-    while (1)
-    {
-        memset(buffer, 0, BUFFER_SIZE);
-        int valread = read(new_socket, buffer, BUFFER_SIZE);
-        if (valread <= 0)
-            break;
-        // Remove newline if present
-        buffer[strcspn(buffer, "\n")] = 0;
-        // Check for termination
-        char command[10], text[BUFFER_SIZE];
-        // Parse input
-        sscanf(buffer, "%[^|]|%[^\n]", command, text);
-        if (strcmp(command, "UP") == 0)
-        {
-            to_upper(text);
-        }
-        else if (strcmp(command, "LOW") == 0)
-        {
-            to_lower(text);
-        }
-        else if (strcmp(command, "REV") == 0)
-        {
-            reverse(text);
-        }
-        else
-        {
-            strcpy(text, "Invalid␣Command");
-        }
-        send(new_socket, text, strlen(text), 0);
-    }
-    close(new_socket);
-    close(server_fd);
-    printf("Server␣terminated.\n");
+        find_student(buffer, response);
+        send(new_socket, response, strlen(response), 0); 
+    } 
+    close(new_socket); 
+    close(server_fd); 
     return 0;
 }
